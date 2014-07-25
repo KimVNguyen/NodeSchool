e0265097b043c66bd568d9d909391d72c770c9b2
Kim Nguyen <kimchimik@gmail.com>
2014-07-24 21:13:12 -0700

    completed

--- LearnYouNode/11-HttpFileServer/HttpFileServer.js
+++ LearnYouNode/11-HttpFileServer/HttpFileServer.js
@@ -1,10 +1,13 @@
 var http = require('http')
+var fs = require('fs')
 
-var port =process.argv[2];
+var port =Number(process.argv[2]);
 var input_file =process.argv[3];
 
 var server = http.createServer(function (req, res) {
-  // request handling logic...
+    // request handling logic...
+    res.writeHead(200, {'content-type': 'text/plain'})
+    fs.createReadStream(input_file).pipe(res)
 })
 server.listen(port)
 
